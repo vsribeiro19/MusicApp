3dc4a1e5d1ec94cee5f1daaffbbd1e6b818fa235
Victor Ribeiro <v.leitteribeiro@gmail.com>
2023-06-20 23:35:55 -0300

    Criando estrutura na Api

---
+++ MusicApp.Api/MusicApp.Api/Controller/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MusicApp.Api.Controller
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class LoginController : ControllerBase
+    {
+
+    }
+}
---
+++ MusicApp.Api/MusicApp.Api/Model/Usuario.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace MusicApp.Api.Model
+{
+    public class Usuario
+    {
+        public string Nome { get; set; }
+
+        public string SobreNome { get; set; }
+
+        public string Login { get; set; }
+
+        public string Email { get; set; }
+
+        public string Senha { get; set; }
+
+    }
+}
