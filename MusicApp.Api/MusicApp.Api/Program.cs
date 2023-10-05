9a32a6b77c94915faa15223c4f7b4795359f81be
Victor Ribeiro <victor.ribeiro@comlink.com.br>
2023-10-05 13:29:54 -0300

    Habilitando o CORS na Api

--- MusicApp.Api/MusicApp.Api/Program.cs
+++ MusicApp.Api/MusicApp.Api/Program.cs
@@ -6,9 +6,17 @@ builder.Services.AddDbContext<DatabaseContext>
     (options => options.UseSqlServer(builder.Configuration.GetConnectionString("MusicApp")));
 var app = builder.Build();
 
+builder.Services.AddCors(option => option.AddDefaultPolicy(policy =>
+{
+    policy.AllowAnyOrigin();
+    policy.AllowAnyMethod();
+    policy.AllowAnyHeader();
+}));
 
 
 
+
+app.UseCors();
 app.MapGet("/", () => "Hello World!"); 
 
 app.Run();
